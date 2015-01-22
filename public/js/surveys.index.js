174b5d1de07f38ca7a31d4a045c560c79f8e7ed5
Jason T. Wong <developer@jasontwong.com>
2015-01-21 22:29:25 -0600

    Added survey details.

--- public/js/surveys.index.js
+++ public/js/surveys.index.js
@@ -4,7 +4,36 @@
     var surveys_charts = $(".surveys-chart"),
       stats = [
         surveys_charts.eq(0).data('stats'),
-      ];
+      ],
+      survey_modal = $('#survey-modal');
+    survey_modal
+      .on('show.bs.modal', function (event) {
+        var el = $(this),
+          row = $(event.relatedTarget),
+          survey = row.data('survey'),
+          store = row.data('store'),
+          member = row.data('member'),
+          body = $('.modal-body', el),
+          survey_info = $('.survey-info .panel-body', body),
+          survey_questions = $('.survey-questions .list-group', body);
+        $('span', survey_info).empty();
+        survey_questions.empty();
+        for (var i in survey.answers) {
+          if (survey.answers.hasOwnProperty(i)) {
+            var ans = survey.answers[i],
+              lg_item = $('<li class="list-group-item" />');
+            lg_item.html('<span class="badge">' + ans.answer + '</span>' + ans.question);
+            survey_questions.append(lg_item);
+          }
+        }
+        $('.store span', survey_info).text(store.name);
+        $('.email span', survey_info).text(member.email);
+        $('.comments span', survey_info).text(survey.comments);
+        $('.worth span', survey_info).text(survey.worth);
+        $('.visited span', survey_info).text(survey.created_at_nice);
+        $('.completed span', survey_info).text(survey.completed_at_nice);
+        $('.nps_score span', survey_info).text(survey.nps_score);
+      });
     new Chart(surveys_charts.get(0).getContext("2d"))
       .Pie([{
           value: stats[0].completed,
